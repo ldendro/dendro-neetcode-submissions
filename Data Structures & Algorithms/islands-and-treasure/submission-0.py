d1a74d6b10cc0fd7313660f3cd3ceb74d84dff3f
ldendro <ldendros@gmail.com>
2026-08-03 13:12:59 -0400

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        ROWS, COLS = len(grid), len(grid[0])
+        seen = set()
+        q = deque()
+
+        def addChest(r, c):
+            if min(r, c) < 0 or r >= ROWS or c >= COLS or (r,c) in seen or grid[r][c] == -1:
+                return
+
+            seen.add((r,c))
+            q.append([r, c])
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 0:
+                    seen.add((r,c))
+                    q.append([r,c])
+
+        dist = 0
+        while q:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                grid[r][c] = dist
+                addChest(r-1, c)
+                addChest(r+1, c)
+                addChest(r, c-1)
+                addChest(r, c+1)
+            dist += 1
